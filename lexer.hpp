525c89aa16158d49c6c92162ce7364afb43acbbf
LiAuTraver <me@liautraver.dev>
2024-10-25 00:40:44 +0800

    finish simple parser, yet so many bugs and code quality problems

--- lexer.hpp
+++ lexer.hpp
@@ -1,28 +1,37 @@
+#pragma once
 #include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <limits>
 #include <map>
 #include <nlohmann/json.hpp>
+#include <print>
 #include <sstream>
 #include <stack>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace net::ancillarycat::waver {
-struct Signal {
-  std::string type;
-  size_t width;
+
+enum class signal_type {
+  kUnknown = 0,
+  kWire = 1,
+  kRegister = 2,
+};
+struct signal {
+  signal_type type = signal_type::kUnknown;
+  size_t width{};
   std::string identifier;
   std::string name;
 };
 
-struct ValueChange {
+struct value_change {
   std::string identifier;
   std::string value;
 };
 
-class Parser {
+class vcd_parser {
 public:
   void parse(const std::string &filename) {
     std::ifstream file(filename);
@@ -32,7 +41,7 @@ public:
     }
 
     std::string line;
-    std::stack<std::string> module_stack;
+    std::vector<std::string> module_stack;
     while (std::getline(file, line)) {
       line = trim(line);
       std::istringstream iss(line);
@@ -43,23 +52,24 @@ public:
         std::string module_type, module_name;
         iss >> module_type >> module_name >> temp;
         if (module_type == "module" && temp == "$end") {
-          module_stack.push(module_name);
+          module_stack.emplace_back(module_name);
         }
       } else if (temp == "$upscope") {
         iss >> temp;
         if (temp == "$end" && !module_stack.empty()) {
-          module_stack.pop();
+          module_stack.pop_back();
+          // test, print the stack
+          for (const auto &module_ : module_stack) {
+            std::cout << module_ << " ";
+          }
+          std::println();
         }
       } else if (temp == "$var") {
         if (!module_stack.empty()) {
-          parse_variable(line, module_stack.top());
+          parse_variable(line, module_stack.back());
         }
       } else if (temp == "$enddefinitions") {
-        // test, print the stack
-				while (!module_stack.empty()) {
-					std::cout << module_stack.top() << std::endl;
-				}
-				break;
+        break;
       }
     }
 
@@ -94,8 +104,8 @@ public:
     nlohmann::json json_data;
 
     // Convert variables and submodules to JSON
-    for (const auto &[module, vars] : module_variables) {
-      auto &module_object = json_data["modules"][module];
+    for (const auto &[vcd_module, vars] : module_variables) {
+      auto &module_object = json_data["modules"][vcd_module];
       for (const auto &[identifier, signal] : vars) {
         module_object["variables"][identifier] = {
             {"type", signal.type},
@@ -103,7 +113,7 @@ public:
             {"reference", signal.name}};
       }
       // Add submodules to the module object
-      for (const auto &submodule : module_submodules[module]) {
+      for (const auto &submodule : module_submodules[vcd_module]) {
         module_object["submodules"].push_back(submodule);
       }
     }
@@ -119,7 +129,7 @@ public:
   }
 
 private:
-  std::unordered_map<std::string, std::unordered_map<std::string, Signal>>
+  std::unordered_map<std::string, std::unordered_map<std::string, signal>>
       module_variables;
   std::map<int, std::unordered_map<std::string, std::string>> signal_values;
   std::unordered_map<std::string, std::vector<std::string>> module_submodules;
@@ -136,17 +146,28 @@ private:
   void parse_variable(const std::string &line, const std::string &module_name) {
     std::istringstream iss(line);
     std::string temp, type, identifier, name;
+    auto signal_type = signal_type::kUnknown;
     long long maybe_width;
     iss >> temp >> type >> maybe_width >> identifier >> name;
+    if (type == "wire") {
+      signal_type = signal_type::kWire;
+    } else if (type == "reg") {
+      signal_type = signal_type::kRegister;
+    }
+
     if (maybe_width < 0)
       maybe_width = std::numeric_limits<long long>::quiet_NaN();
     module_variables[module_name][identifier] = {
-        type, static_cast<size_t>(maybe_width), identifier, name};
+        signal_type, static_cast<size_t>(maybe_width), identifier, name};
   }
 
   void parse_value_change(const std::string &line) {
     if (line[0] == '#') {
-      current_time = std::stoi(line.substr(1));
+      if (auto [_, ec] = std::from_chars(
+              line.data() + 1, line.data() + line.size(), current_time);
+          ec != std::errc()) {
+        std::cerr << "Failed to parse time: " << line.substr(1) << std::endl;
+      }
     } else if (line[0] == 'b') {
       size_t space_pos = line.find(' ');
       std::string value = line.substr(1, space_pos - 1);
--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include "lexer.hpp"
+#include <filesystem>
 #include <fstream>
 #include <nlohmann/json.hpp>
-int main() {
-  net::ancillarycat::waver::Parser parser;
-  parser.parse(R"(Z:\Cpp-Playground\waver\test\ALU4.vcd)");
+int main(int argc, char *argv[]) {
+  net::ancillarycat::waver::vcd_parser parser;
+  std::filesystem::path source_file;
+  std::filesystem::path output_file;
+  if (argc == 1) {
+    source_file = R"(Z:\Cpp-Playground\waver\test\ALU4.vcd)";
+    output_file = R"(Z:\Cpp-Playground\waver\test\ALU4.json)";
+  }
+	if (argc == 2) {
+		source_file = argv[1];
+		output_file = source_file.replace_extension(".json");
+	}
+	if (argc == 3) {
+		source_file = argv[1];
+		output_file = argv[2];
+	}
+
+  parser.parse(source_file.string());
   parser.print_parsed_data();
 
-  nlohmann::json jsonData = parser.to_json();
-  std::ofstream outFile(R"(Z:\Cpp-Playground\waver\test\ALU4.json)");
-  outFile << jsonData.dump(4);
-  outFile.close();
-
+  const auto jsonData = parser.to_json();
+  std::ofstream outFile(output_file);
+  outFile << jsonData.dump(2);
   return 0;
 }
